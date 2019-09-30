b230207ad0f20425291671d8155e7c2e5e06c959
Pascal Luckhaus <pascal.luckhaus@edu.fh-joanneum.at>
2019-09-30 12:33:09 +0200

    Fix port variable

--- index.js
+++ index.js
@@ -78,5 +78,5 @@ app.get('/info', (request, response) => {
   response.send(html);
 });
 
-const PORT = process.event.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
