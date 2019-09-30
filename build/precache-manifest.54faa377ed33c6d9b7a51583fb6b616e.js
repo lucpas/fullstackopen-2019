6b7f0353d482e3fb4d44255c8f990a14bc91002c
Pascal Luckhaus <pascal.luckhaus@edu.fh-joanneum.at>
2019-09-30 13:56:11 +0200

    uibuild

---
+++ build/precache-manifest.54faa377ed33c6d9b7a51583fb6b616e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f2a552108fd3e5e8f26894094b8840",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63f40630c89d0da4dc48",
+    "url": "/static/css/main.e3092b65.chunk.css"
+  },
+  {
+    "revision": "5e14a20fbc437a35d975",
+    "url": "/static/js/2.b10f7662.chunk.js"
+  },
+  {
+    "revision": "63f40630c89d0da4dc48",
+    "url": "/static/js/main.ca458469.chunk.js"
+  },
+  {
+    "revision": "63932adef59fccfa75c2",
+    "url": "/static/js/runtime-main.1c1f58c9.js"
+  }
+]);
